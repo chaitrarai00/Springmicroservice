24295bc9bac23bf8f48275ac9704aed5ee4eff0c
chaitrarai00 <32186609+chaitrarai00@users.noreply.github.com>
2021-06-20 20:07:25 +0530

    movieinfo and ratings service

--- ratings-data-service/ratings-data-service/src/main/java/com/ratingsdataservice/demo/RatingsDataServiceApplication.java
+++ ratings-data-service/ratings-data-service/src/main/java/com/ratingsdataservice/demo/RatingsDataServiceApplication.java
@@ -2,7 +2,9 @@ package com.ratingsdataservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.ratingsdataservice")
 @SpringBootApplication
 public class RatingsDataServiceApplication {
 
