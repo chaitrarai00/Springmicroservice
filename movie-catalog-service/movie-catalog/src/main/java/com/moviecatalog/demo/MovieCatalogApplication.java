d33cc609f2828b561ed70cd786eda5fa90100c55
chaitrarai00 <32186609+chaitrarai00@users.noreply.github.com>
2021-06-20 18:53:04 +0530

    adding movie-catalog service

--- movie-catalog-service/movie-catalog/src/main/java/com/moviecatalog/demo/MovieCatalogApplication.java
+++ movie-catalog-service/movie-catalog/src/main/java/com/moviecatalog/demo/MovieCatalogApplication.java
@@ -2,7 +2,9 @@ package com.moviecatalog.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.moviecatalog.resources")
 @SpringBootApplication
 public class MovieCatalogApplication {
 
